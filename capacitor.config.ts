24083d7854709efb1f216044cbf68e132991aad9
Goodness <ebulug001@gmail.com>
2026-07-08 09:53:09 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.king.swiftpdf',
+  appName: 'SwiftPdf',
+  webDir: 'www'
+};
+
+export default config;
